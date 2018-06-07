78343bb703a6a25bbe36dea9a68fdaaebde9ee46
jean-yves <jean-yves.lecabellec@leetchi.com>
2018-06-07 13:20:21 +0200

    Creating a Server in Express

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 const express = require('express');
+// Create exrpess server (add it to a const)
+const app = express();
+// Listen to port 3000
+app.listen(3000);
+// Go to 127.0.0.1:3000, server is ready !
