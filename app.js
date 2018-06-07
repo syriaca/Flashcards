1b7387e1ad2cc661ac1b4279cc8fc39dfbc236b4
jean-yves <jean-yves.lecabellec@leetchi.com>
2018-06-07 16:20:08 +0200

    Adding Multiple Routes to the App

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const express = require('express');
 // Create exrpess server (add it to a const)
 const app = express();
 // Create routes
-app.get('/', (request, response) => {
-    response.send('I love coding !');
+app.get('/', (req, res) => {
+    res.send('<h1>I love coding !</h1>');
+});
+app.get('/hello', (req, res) => {
+    res.send('<h1>Hello Javascript Developper !</h1>');
 });
 // Listen to port 3000
-app.listen(3000);
+app.listen(3000, () => {
+    console.log('the application is running now on localhost:3000');
+});
 // Go to 127.0.0.1:3000, server is ready !
