25adc96165d4e886c117b010515106099f0de25a
jean-yves <jean-yves.lecabellec@leetchi.com>
2018-06-07 13:29:26 +0200

    Creating a route & added nodemon so, change idnex.js from package.json

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 // Create exrpess server (add it to a const)
 const app = express();
+// Create routes
+app.get('/', (request, response) => {
+    response.send('I love coding !');
+});
 // Listen to port 3000
 app.listen(3000);
 // Go to 127.0.0.1:3000, server is ready !
