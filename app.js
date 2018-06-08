0402a50a4b1ca462072f8c7c948805888e1675d7
jean-yves <jean-yves.lecabellec@leetchi.com>
2018-06-08 11:31:14 +0200

    Added pug template rendering and first pug view

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const express = require('express');
+
 // Create exrpess server (add it to a const)
 const app = express();
+
+// Set view renderer engine
+app.set('view engine', 'pug');
+
 // Create routes
 app.get('/', (req, res) => {
-    res.send('<h1>I love coding !</h1>');
+    res.render('index');
 });
+
 app.get('/hello', (req, res) => {
     res.send('<h1>Hello Javascript Developper !</h1>');
 });
+
 // Listen to port 3000
 app.listen(3000, () => {
     console.log('the application is running now on localhost:3000');
 });
+
 // Go to 127.0.0.1:3000, server is ready !
