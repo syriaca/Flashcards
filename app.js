a1e480421a7c48f61928ffb9e59732c9f1784568
jean-yves <jean-yves.lecabellec@leetchi.com>
2018-06-11 11:53:57 +0200

    Breaking Project’s Templates into Partials

--- app.js
+++ app.js
@@ -3,17 +3,31 @@ const express = require('express');
 // Create exrpess server (add it to a const)
 const app = express();
 
+// Sandbox values
+friends =  [
+    ['Halim','El Maalem'],
+    ['Julien', 'Bareaud'],
+    ['Fabien','Jacques'],
+    ['Vivien', 'Le Neez'],
+    ['Cécile', 'Perier']
+];
+
 // Set view renderer engine
 app.set('view engine', 'pug');
 
 // Create routes
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', {title: 'Flash Cards Index'});
 });
 
-app.get('/hello', (req, res) => {
-    res.send('<h1>Hello Javascript Developper !</h1>');
+app.get('/card', (req, res) => {
+    res.render('card', {prompt: 'Who is buried in Grant\'s tomb ?', hint: 'Remembar blah, blah,blah', title: 'Flash Card'});
 });
+
+app.get('/sandbox', (req, res) => {
+    res.render('sandbox', {friends, title: 'Sandbox'});
+});
+
 
 // Listen to port 3000
 app.listen(3000, () => {
