26f70dfbf53488fa1ede8340228dac64294bc4ba
jean-yves <jean-yves.lecabellec@leetchi.com>
2018-06-18 12:09:27 +0200

    Adding link
    Added a link to flip card (answer or question)

--- routes/cards.js
+++ routes/cards.js
@@ -6,12 +6,17 @@ const { cards } = data;
 router.get('/:id', (req, res) => {
     const { side } = req.query;
     const { id } = req.params;
-    const text = cards[id][side];
     const { hint } = cards[id];
-    const templateData = { text, hint };
-
+    const text = cards[id][side];
+    let sideToShow = 'answer';
+    let templateData = { text, id, sideToShow };
+    
+    if(side === 'question') {
+        templateData.hint = hint;
+    } else {
+        templateData.sideToShow = 'question';
+    }
     res.render('card', templateData);
-    });
 });
 
 module.exports = router;
