abe4d141c4b62da56ffef4d55dfae69f3d5b5276
jean-yves <jean-yves.lecabellec@leetchi.com>
2018-06-19 11:01:52 +0200

    Added some route improvement and mutualize goodbye button

--- routes/cards.js
+++ routes/cards.js
@@ -6,21 +6,22 @@ const { cards } = data;
 router.get('/', (req, res) => {
     const numberOfCards = cards.length;
     const flashCardId = Math.floor(Math.random() * numberOfCards);
-    res.redirect(`/cards/${flashCardId}?side=question`);
-});
-
-router.get('/', (req, res) => {
-    const { id } = req.params;  
-    res.redirect(`/cards/${id}?side=question`);
+    res.redirect(`/cards/${flashCardId}`);
 });
 
 router.get('/:id', (req, res) => {
     const { side } = req.query;
     const { id } = req.params;
+
+    if(!side) {
+        res.redirect(`/cards/${id}?side=question`);
+    }
+
+    const name = req.cookies.username;
     const { hint } = cards[id];
     const text = cards[id][side];
     let sideToShow = 'answer';
-    let templateData = { text, id, sideToShow };
+    let templateData = { text, id, sideToShow, name };
 
     if(side === 'question') {
         templateData.hint = hint;
