ed1d860203f3c64dc7131a98d9c41857e9c2357e
Jean-Yves Le Cabellec <jy.lecabellec@gmail.com>
2018-06-19 09:47:43 +0200

    Added new route redirection with random question

--- routes/cards.js
+++ routes/cards.js
@@ -3,6 +3,17 @@ const router = express.Router();
 const { data } = require('../data/flaschardsData.json');
 const { cards } = data;
 
+router.get('/', (req, res) => {
+    const numberOfCards = cards.length;
+    const flashCardId = Math.floor(Math.random() * numberOfCards);
+    res.redirect(`/cards/${flashCardId}?side=question`);
+});
+
+router.get('/', (req, res) => {
+    const { id } = req.params;  
+    res.redirect(`/cards/${id}?side=question`);
+});
+
 router.get('/:id', (req, res) => {
     const { side } = req.query;
     const { id } = req.params;
@@ -10,7 +21,7 @@ router.get('/:id', (req, res) => {
     const text = cards[id][side];
     let sideToShow = 'answer';
     let templateData = { text, id, sideToShow };
-    
+
     if(side === 'question') {
         templateData.hint = hint;
     } else {
